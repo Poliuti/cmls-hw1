befa4b466792864e39da94d8115caf8405330bea
Matteo Bernardini <mat_teobe@yahoo.it>
2020-04-15 12:29:07 +0200

    fix: download on proper location and check if already downloaded

--- project/bin/download_dataset.py
+++ project/bin/download_dataset.py
@@ -2,9 +2,16 @@ from requests import get
 from shutil import copyfileobj
 from zipfile import ZipFile
 from os import unlink
+from os.path import isdir
+from sys import exit
 
 URL  = "https://polimi365-my.sharepoint.com/:u:/g/personal/10768481_polimi_it/ET_EMOV_tgBAm2yIQn4m4h0B8FxvxcDCJkpedf_3SRtLWw?download=1"
-NAME = "../dataset.zip"
+NAME = "./dataset.zip"
+
+## Check first if already extracted
+if isdir(NAME[:-4]):
+	print("already downloaded.")
+	exit(0)
 
 ## download
 print("downloading...")
