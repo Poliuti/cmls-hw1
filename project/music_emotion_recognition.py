44ac54f65e2f4679a18fda8af359bb8642dbc4c5
Matteo Bernardini <mat_teobe@yahoo.it>
2020-04-20 20:37:52 +0200

    some feature visualization

--- project/music_emotion_recognition.py
+++ project/music_emotion_recognition.py
@@ -13,7 +13,7 @@ import sklearn.model_selection
 import sklearn.linear_model
 import sklearn.svm
 
-from tqdm import tqdm
+from tqdm.notebook import tqdm
 from functools import lru_cache
 from operator import itemgetter
 from zipfile import ZipFile
@@ -53,72 +53,88 @@ def get_frame_level_features(track_id):
     with open(os.path.join(DATASET_PATH, "features", f"{track_id}.csv")) as fin:
         return pd.read_csv(fin, header=0, index_col=0, sep=";", engine="c")
 
-def feature_selector(name):
-    return "zcr" in name or "F0" in name
-    
-@lru_cache(maxsize=None)
-def get_features(track_id):
-    """returns a pandas matrix of selected features for track `track_id`"""
-    feats = get_frame_level_features(track_id)
-    #return pd.concat((feats.mean(), feats.std()), keys=["mean", "std"], axis=1, copy=False)
-    # TODO: how to extract clip-level features from time-level features averaged with a moving average of 3?
-    return feats.loc[:, filter(feature_selector, feats.columns)].mean()
+def get_clip_level_features(track_id):
+    """converts frame-level features to relevant clip-level features"""
+    # just mean everything for now, will be redefined later in the notebook
+    sr = get_frame_level_features(track_id).mean()
+    sr.name = track_id
+    return sr
 
-def get_all_features(length=None):
-    """iterates over the dataset and extracts features of all tracks"""
-    all_feats = []
-    track_files = os.listdir(os.path.join(DATASET_PATH, "features/"))
-    selected_tracks = sorted(map(lambda name: int(name.split(".")[0]), track_files))[:length]
-    for track_id in tqdm(selected_tracks):
-        feats = get_features(track_id)
-        all_feats.append((track_id, feats))
-    # NB: the upper limit is because we are only interested to the `2-2000` range.
-    return pd.DataFrame(map(itemgetter(1), all_feats), index=map(itemgetter(0), all_feats)).loc[:2000]
+def get_features(selected_tracks=None, length=None):
+    """iterates over the dataset and return a pandas matrix of features for all/selected tracks"""
+    if selected_tracks is None:
+        track_files = os.listdir(os.path.join(DATASET_PATH, "features/"))
+        selected_tracks = sorted(map(lambda name: int(name.split(".")[0]), track_files))[:length]
+    all_feats = (get_clip_level_features(track_id) for track_id in tqdm(selected_tracks))
+    # NB: the upper limit is set because we are only interested to the `2-2000` range.
+    return pd.DataFrame(all_feats).loc[:2000]
 # -
 
-get_all_features(10)
+get_features(length=100)
 
 # ## Extract Annotations
 
-@lru_cache(maxsize=1)
-def get_all_annotations(length=None):
+def get_annotations(length=None):
     """returns a pandas matrix of all the annotations of all tracks"""
     with open(os.path.join(DATASET_PATH, "annotations.csv")) as fin:
         return pd.read_csv(fin, header=0, index_col=0, sep=",\s*", engine="python").iloc[:length]
 
-get_all_annotations(10)
+get_annotations(50)
 
 
 # ## Feature Visualization
 
-some_feats = get_frame_level_features(115)
-F = some_feats.loc[:, "pcm_fftMag_spectralHarmonicity_sma_amean"]
-some_feats
-
-print(some_feats.loc[:, "F0final_sma_amean"].mean())
-print(some_feats.loc[:, "F0final_sma_stddev"].mean())
-
-plt.plot(F)
-
-# ### Valence characteristics
+# ### Annotations splitting
 
 # +
-tid_max_valence = get_all_annotations().loc[:, "valence_mean"].idxmax()
-tid_min_valence = get_all_annotations().loc[:, "valence_mean"].idxmin()
+maxs = dict()
+mins = dict()
 
-pd.DataFrame((get_features(tid_max_valence), get_features(tid_min_valence)), index=["max", "min"])
-# -
-
-# ### Arousal characteristics
-
-# +
-tid_max_arousal = get_all_annotations().loc[:, "arousal_mean"].idxmax()
-tid_min_arousal = get_all_annotations().loc[:, "arousal_mean"].idxmin()
-
-pd.DataFrame((get_features(tid_max_arousal), get_features(tid_min_arousal)), index=["max", "min"])
+for label in get_annotations().columns:
+    annot = get_annotations().loc[:, label]
+    maxs[label] = annot.loc[annot >= annot.mean()].sort_values(ascending=False).index
+    mins[label] = annot.loc[annot < annot.mean()].sort_values(ascending=True).index
 
 
 # -
+
+def plot_feature_evolution(tracks, feature_name, time_slice=slice(None)):
+    data = pd.concat((
+        get_frame_level_features(i).loc[time_slice, feature_name]
+        for i in tqdm(tracks)
+        ), axis=1)
+    plt.xlabel("time")
+    plt.ylabel(feature_name)
+    plt.plot(data)
+
+
+# ### Feature names
+
+with open("features.txt") as fin:
+    print(fin.read())
+
+# ### Harmonicity
+
+# Plotting Harmonicity evolution for the first 5 tracks with **maximum**/**minumum** valence.
+
+plt.figure(figsize=(15,5))
+plt.subplot(1,2,1)
+plt.title("tracks with min. valence mean")
+plot_feature_evolution(mins["valence_mean"][:5], "pcm_fftMag_spectralHarmonicity_sma_amean", slice(10,50))
+plt.subplot(1,2,2)
+plt.title("tracks with max. valence mean")
+plot_feature_evolution(maxs["valence_mean"][:5], "pcm_fftMag_spectralHarmonicity_sma_amean", slice(10,50))
+
+# Plotting Harmonicity evolution for the first 5 tracks with **maximum**/**minumum** arousal.
+
+plt.figure(figsize=(15,5))
+plt.subplot(1,2,1)
+plt.title("tracks with min. arousal mean")
+plot_feature_evolution(mins["arousal_mean"][:5], "pcm_fftMag_spectralHarmonicity_sma_amean", slice(10,50))
+plt.subplot(1,2,2)
+plt.title("tracks with max. arousal mean")
+plot_feature_evolution(maxs["arousal_mean"][:5], "pcm_fftMag_spectralHarmonicity_sma_amean", slice(10,50))
+
 
 # # Regression
 
@@ -139,9 +155,9 @@ def predict_regressor(reg, features):
 
 # Extract N tracks from the dataset.
 
-N       = 100
-feats   = get_all_features(N)
-annots  = get_all_annotations(N)
+N       = 2000
+feats   = get_features(length=N)
+annots  = get_annotations(length=N)
 
 # Split the dataset in training set and testing set.
 
